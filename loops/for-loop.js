903607b78319a2a1a4212c0302f39339ed3a2399
mordini <yahewisthedevil@yahoo.com>
2023-09-29 15:32:13 +0000

    added for loop

---
+++ loops/for-loop.js
@@ -0,0 +1,50 @@
+// READ THIS TUTORIAL LINE BY LINE AND IT SHOULD MAKE SENSE
+
+
+// THIS IS A 'FOR' LOOP, named for the...'for'.
+for (let count = 0; count < 10; count++) {
+  console.log(i);
+}
+
+
+
+/*
+ // IT IS CONSTRUCTED LIKE SO
+
+
+for (initialExpression; condition; updateExpression) {
+    // FOR LOOP BODY
+
+    // do things here
+    // like make a pretty girl wave hello.
+    // does she keep waving like a lunatic, making you concerned,
+    // or wave three times, then you go out for coffee?
+
+}
+
+ HOW TO READ IT IN ENGLISH (like a sentence)
+
+  for (let count = 0;
+          for our 'count' is equal to zero;  (starting from 0)
+      count < 10;
+          as long as 'count' is less than 10;  (keep going until we count to ten)
+      count++)
+          'count' up by one more number (we haven't reached ten yet, so we need to keep counting)
+
+  {
+    // PUT THINGS TO DO INSIDE OPENING AND CLOSING CURLY BRACES
+    // every time we count to one, things happen in here
+    // like jumping rope.
+    // you start at 0.
+    // you jump the rope, that's one.
+    // jump again, two...until you jump ten times
+  }
+
+*/
+
+
+// CREATE AN ARRAY TO HOLD A LIST OF ANIMALS
+
+// ADD A LIST OF ANIMALS TO THE ARRAY
+
+// CREATE A FOR LOOP TO LIST
